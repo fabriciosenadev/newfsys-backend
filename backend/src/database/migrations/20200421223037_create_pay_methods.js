9e934e7a8d897efec3f8b972c34ccae185ac2063
Fabricio Sena <fabricior.sena@outlook.com>
2020-04-21 22:58:53 -0300

    fix fields to not nullable

--- backend/src/database/migrations/20200421223037_create_pay_methods.js
+++ backend/src/database/migrations/20200421223037_create_pay_methods.js
@@ -2,8 +2,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable('pay_methods', function (table) {
     table.increments();
-    table.string('pay_method');
-    table.enum('applicable', ['wallet','account','credit']);
+    table.string('pay_method').notNullable();
+    table.enum('applicable', ['wallet','account','credit']).notNullable();
   });
 };
 
