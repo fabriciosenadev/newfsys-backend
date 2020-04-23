2a4c6854cde72bd22ceeb01fc931595e8f768f77
Fabricio Sena <fabricior.sena@outlook.com>
2020-04-22 23:16:22 -0300

    add method to register new user

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -3,8 +3,23 @@ const connection = require('../database/connection');
 module.exports = {
     async store (request, response) 
     {
-        const body = request.body;
-        response.json(body);
+        try
+        {
+            const { full_name, email, password } = request.body;
+                        
+            await connection('fsys_users').insert({
+                full_name,
+                email,
+                password,
+                created_at: new Date().toISOString()
+            });
+            
+            return response.json({ success:"data was saved with successfully" });
+        }
+        catch (error)
+        {
+            return response.json({ error });
+        }
     },
 
     async login (request, response)
