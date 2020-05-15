e1a5f9873efe9409f5f88920f4b9e845f2fc1f9b
Fabricio Sena <fabricior.sena@outlook.com>
2020-05-15 10:34:16 -0300

    add jwt and dotenv-safe dependencies and modify port comes from

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
+
+// obtain data from .env file
+require('dotenv-safe').config({
+    allowEmptyValues: true
+});
+
+// route files
 const routes = require('./routes');
+
+// API start point
 const api = express();
 
+// access security
 api.use(cors());
 
+// allow to use json format
 api.use(express.json());
+
+// use all routes declared
 api.use(routes);
 
-api.listen(3040);
+// use port parameter from .env file if has port
+api.listen(process.env.PORT || 3040);
