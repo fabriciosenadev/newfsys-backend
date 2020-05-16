66b35eee66f99941b4a116be3282b2fc9cc67c16
Fabricio Sena <fabricior.sena@outlook.com>
2020-05-16 19:06:14 -0300

    improve code of index method

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -3,11 +3,20 @@ const connection = require('../database/connection');
 module.exports = {
     async index (request, response) 
     {
-        const userId = request.headers.authorization;
-        const launching = await connection('fsys_historics')
-        .where('created_by', userId)
-        .select('*');
+        try
+        {
+            const { userId } = request.body;
 
-        return response.json(launching);
+            const launching = await connection('fsys_historics')
+                                        .where('created_by', userId)
+                                        .orderBy('date', 'asc')
+                                        .select('*')
+
+            return response.json({ launching });
+        }
+        catch (error)
+        {
+            return response.json(error);
+        }
     },
 };
