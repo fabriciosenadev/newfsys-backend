dc7788cc956f4bef72c47ea0d481751b694013b1
Fabricio Sena <fabricior.sena@outlook.com>
2020-05-19 20:02:57 -0300

    add store method to in or out action

--- backend/src/controllers/LaunchController.js
+++ backend/src/controllers/LaunchController.js
@@ -1,11 +1,45 @@
 const connection = require('../database/connection');
 
 module.exports = {
-    async index (request, response) 
+    async store (request, response) 
     {
-        const userId = request.headers.authorization;
-        const launching = '';
+        try
+        {
+            const { date, description, value, idCategory, userId, idPayMethod } = request.body;
+            
+            const idHistoric = await connection('fsys_historics')
+                                    .insert({
+                                        date,
+                                        description,
+                                        value,
+                                        id_category: idCategory,
+                                        created_by: userId,
+                                        created_at: new Date().toISOString()
+                                    });
 
-        response.send('launching is working');
-    }
+            if(!idHistoric)
+            {
+                return response.status(507).json({ 
+                    error: "An error ocurred when we tried to save data"
+                });
+            }
+
+            if(idPayMethod)
+            {
+                await connection('fsys_pay_method_historics')
+                    .insert({
+                        id_pay_method: idPayMethod,
+                        id_historic: idHistoric[0],
+                        created_by: userId                        
+                    });
+            }
+            
+            return response.status(200).json({ success:"data was saved with successfully" });
+            
+        }
+        catch (error) 
+        {
+            return response.status(500).json({ error });
+        }
+    },
 };
