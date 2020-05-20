b89946768d4ff63d3b7e28925fbf2306b9adf53f
Fabricio Sena <fabricior.sena@outlook.com>
2020-05-20 00:30:02 -0300

    add show launch method

--- backend/src/controllers/LaunchController.js
+++ backend/src/controllers/LaunchController.js
@@ -43,4 +43,30 @@ module.exports = {
             return response.status(500).json({ error });
         }
     },
+
+    async show (request, response) 
+    {
+        try
+        {
+
+            const { id } = request.params;
+            const { userId } = request.body;
+
+            const launchData = await connection('fsys_historics AS h')
+                                .select('h.id','h.date','h.description','h.value', 'c.category','pm.pay_method')
+                                .join('fsys_categories AS c','h.id_category','c.id')
+                                .leftJoin('fsys_pay_method_historics AS pmh', 'h.id', 'pmh.id_historic')
+                                .leftJoin('fsys_pay_methods AS pm', 'pmh.id_pay_method', 'pm.id')
+                                .where('h.id','=',id, 'and', 'h.created_by', '=', userId)
+                                .whereNull('h.deleted_at')
+                                .first();
+            
+            console.log(launchData);
+            return response.status(200).json({ launchData });
+        }
+        catch(error)
+        {
+            return response.status(500).json({ error });
+        }
+    },
 };
