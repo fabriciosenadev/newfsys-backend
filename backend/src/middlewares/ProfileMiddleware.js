0d2b2ebe22fbafa566a2617a0715252a98e2a6be
Fabricio Sena <fabricior.sena@outlook.com>
2020-05-21 13:36:01 -0300

    add method to check date sent to filter launches

---
+++ backend/src/middlewares/ProfileMiddleware.js
@@ -0,0 +1,25 @@
+const moment = require('moment');
+
+exports.checkDate = async (request, response, next) => {
+
+    const { fromDate, toDate } = request.body;
+    
+    const FromDate = moment(fromDate, 'YYYY-MM-DD', true).isValid();
+    const ToDate = moment(toDate, 'YYYY-MM-DD', true).isValid();
+
+    if (!FromDate || !ToDate) {
+        return response.status(422).json({ 
+            errors: "some date is invalid please verify and try again",
+            datesValidation: { FromDate, ToDate }
+        });
+    }
+
+    const diff = moment(fromDate).isAfter(toDate);
+
+    if(diff)
+    {
+        return response.status(422).json({ error: "date from cannot be greater than date to" });
+    }
+
+    next();
+};
