52c96c8b0cf22f5b3e5d8b03e3b579f51733bf13
Fabricio Sena <fabricior.sena@outlook.com>
2020-05-21 14:43:38 -0300

    add 'update' method to validate data

--- backend/src/middlewares/LaunchMiddleware.js
+++ backend/src/middlewares/LaunchMiddleware.js
@@ -37,3 +37,40 @@ exports.validateStore = async (request, response, next) => {
 
     next();
 };
+
+exports.validateUpdate = async (request, response, next) => {
+    const { date, idPayMethod } = request.body;
+
+    const isDate = moment(date, 'YYYY-MM-DD', true).isValid();
+
+    if(!isDate)
+    {
+        return response.status(422).json({ error: "date is required, please verify if it's correctly" });
+    }
+
+    await check('value')
+        .exists()
+        .withMessage('value is required')
+        .isFloat()
+        .withMessage('value is not valid');
+
+    await check('idCategory')
+        .exists()
+        .withMessage("category id is required")
+        .isInt()
+        .withMessage('category id is not valid');
+
+    if(idPayMethod)
+    {
+        await check('idPayMethod')
+            .isInt()
+            .withMessage('pay method id is not valid');
+    }
+
+    const result = validationResult(request);
+    if (!result.isEmpty()) {
+        return response.status(422).json({ errors: result.array() });
+    }
+
+    next();
+};
