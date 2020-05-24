2cbbc3940e9d6f7359fc43cf45d6b0dedad53e29
Fabricio Sena <fabricior.sena@outlook.com>
2020-05-24 15:24:29 -0300

    add about page

---
+++ front-react/src/pages/site/About/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../template/Navbar';
+
+import './styles.css';
+
+export default function About()
+{
+    return(
+        <div>        
+            <Navbar />
+            <h1>About - Fsys</h1>
+        </div>
+    );
+}
