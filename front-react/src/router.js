5fb278a4a996ce71060879061578f4bb128fef01
Fabricio Sena <fabricior.sena@outlook.com>
2020-05-24 15:22:57 -0300

    add about route

--- front-react/src/router.js
+++ front-react/src/router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // site
 import Home from './pages/site/Home';
+import About from './pages/site/About';
 
 export default function Routes()
 {
@@ -10,6 +11,7 @@ export default function Routes()
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home}/>
+                <Route path="/about" component={About} />
             </Switch>
         </BrowserRouter>
     );
