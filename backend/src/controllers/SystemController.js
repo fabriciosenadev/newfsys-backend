6483d57f05009323965113a4f89005429cb1dba4
Fabricio Sena <fabricior.sena@outlook.com>
2020-07-05 04:17:42 -0300

    add getMonth method

--- backend/src/controllers/SystemController.js
+++ backend/src/controllers/SystemController.js
@@ -42,5 +42,55 @@ module.exports = {
         } catch (error) {
             return response.status(500).json({ error });
         }
+    },
+
+    async getMonth(request, response) {
+        try {
+            const { userId } = request.body;            
+            const { year, month } = request.params;
+
+            let lastDay = '30';
+            // janeiro, março, maio, julho, agosto, outubro, dezembro
+            switch(month)
+            {
+                case '01':
+                case '03':
+                case '05':
+                case '07':
+                case '08':
+                case '10':
+                case '12':
+                    lastDay = '31';
+                    break;
+                case '02':
+                    lastDay = '28';
+                    break;
+            }
+
+            let fromDate = `${year}-${month}-01`;
+            let toDate = `${year}-${month}-${lastDay}`;               
+            
+            const received = await connection('fsys_historics AS h')
+                .select('h.value')
+                .sum('h.value as value')
+                .join('fsys_categories AS c','h.id_category','c.id')
+                .where('h.created_by', userId)
+                .andWhere('c.applicable','in')
+                .andWhereBetween('date',[fromDate, toDate])
+                .whereNull('h.deleted_at');
+                
+            const paid = await connection('fsys_historics AS h')
+                .select('h.value')
+                .sum('h.value as value')
+                .join('fsys_categories AS c','h.id_category','c.id')
+                .where('h.created_by', userId)
+                .andWhere('c.applicable','out')
+                .andWhereBetween('date',[fromDate, toDate])
+                .whereNull('h.deleted_at');
+
+            return response.status(200).json({ received, paid });
+        } catch (error) {
+            return response.status(500).json({ error });
+        }
     }
 };
