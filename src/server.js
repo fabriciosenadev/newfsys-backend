f9210353a2d806c5cd10022860b3ddb97f91b255
Fabricio Sena <fabricior.sena@outlook.com>
2020-07-07 00:53:41 -0300

    fix cors

--- src/server.js
+++ src/server.js
@@ -15,10 +15,7 @@ const routes = require('./routes');
 const api = express();
 
 // access security
-api.use(cors({
-    origin: 'https://fabriciosenadev.github.io',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}));
+api.use(cors());
 
 // allow to use json format
 api.use(express.json());
