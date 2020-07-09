7ea0b70734d94036aaafd2f8d0a61b15938236fc
fabriciosenadev <39681954+fabriciosenadev@users.noreply.github.com>
2020-07-09 00:39:40 -0300

    fix method to validate data to save launch (#2)

--- src/middlewares/LaunchMiddleware.js
+++ src/middlewares/LaunchMiddleware.js
@@ -2,39 +2,40 @@ const moment = require('moment');
 const { check, oneOf, validationResult } = require('express-validator');
 
 exports.validateStore = async (request, response, next) => {
-    const { date, idPayMethod } = request.body;
-
+    const { date, id_pay_method } = request.body;
     const isDate = moment(date, 'YYYY-MM-DD', true).isValid();
 
     if(!isDate)
     {
-        return response.status(422).json({ error: "date is required, please verify if it's correctly" });
+        return response.status(422).json({ msg: "date is required, please verify if it's correctly" });
     }
 
     await check('value')
         .exists()
         .withMessage('value is required')
-        .isFloat()
-        .withMessage('value is not valid');
+        .isFloat({ gt: 0.0 })
+        .withMessage('value is not valid')
+        .run(request);
 
-    await check('idCategory')
+    await check('id_category')
         .exists()
         .withMessage("category id is required")
-        .isInt()
-        .withMessage('category id is not valid');
+        .isInt({ gt: 0})
+        .withMessage('category id is not valid')
+        .run(request);
 
-    if(idPayMethod)
+    if(id_pay_method !== undefined)
     {
-        await check('idPayMethod')
-            .isInt()
-            .withMessage('pay method id is not valid');
+        await check('id_pay_method')
+            .isInt({ gt: 0})
+            .withMessage('pay method id is not valid')
+            .run(request);
     }
 
     const result = validationResult(request);
     if (!result.isEmpty()) {
         return response.status(422).json({ data: result.array() });
     }
-
     next();
 };
 
